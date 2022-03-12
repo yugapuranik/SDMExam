672a356c3f8ae5d1ec5c8a9935721199895dab75
yuga9880 <yugapuranik17@gmail.com>
2022-03-12 14:39:55 +0530

    updated

---
+++ Java_Project/Operations/src/MathematicalOperations.java
@@ -0,0 +1,14 @@
+
+public class MathematicalOperations {
+
+	public static void main(String[] args) {
+	
+		int a=10;
+		int b=20;
+		
+		int c=a+b;
+		
+		System.out.println("addition is" +c);
+	}
+
+}
